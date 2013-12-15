ac48efe7bd14c04d757b005dc2ba3d1ff641c256
amjadKhalifah <amjad.khalifah@gmail.com>
2013-12-15 18:14:35 +0100

    Added ECS serialization util functions and added unit tests

--- storage/src/client/SerializationUtil.java
+++ storage/src/client/SerializationUtil.java
@@ -1,64 +1,170 @@
 package client;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.activation.UnsupportedDataTypeException;
+
+import app_kvEcs.ECSCommand;
 import app_kvEcs.ECSMessage;
 import app_kvServer.ServerMessage;
+import common.ServerInfo;
 import common.messages.AbstractMessage;
+import common.messages.AbstractMessage.MessageType;
 import common.messages.KVMessage.StatusType;
 import common.messages.KVMessage;
 import common.messages.ClientMessage;
 
 public class SerializationUtil {
 
-	private static final String LINE_FEED = "&&";
-	private static final char RETURN = 0x0D;
+    private static final String LINE_FEED = "&&";
+    private static final String INNER_LINE_FEED = "##";
+    private static final String INNER_LINE_FEED2 = "%%";
+    private static final String ECS_MESSAGE = "0";
+    private static final String SERVER_MESSAGE = "1";
+    private static final String CLIENT_MESSAGE = "2";
 
-	public static byte[] toByteArray(KVMessage message) {
+    private static final char RETURN = 0x0D;
 
-		// message : number(0)$key$value
+    public static byte[] toByteArray(KVMessage message) {
 
-		byte[] bytes = (message.getStatus().ordinal() + LINE_FEED
-				+ message.getKey() + LINE_FEED + message.getValue()).getBytes();
-		byte[] ctrBytes = new byte[] { RETURN };
-		byte[] tmp = new byte[bytes.length + ctrBytes.length];
-		System.arraycopy(bytes, 0, tmp, 0, bytes.length);
-		System.arraycopy(ctrBytes, 0, tmp, bytes.length, ctrBytes.length);
-		return tmp;
-	}
+	// message : number(0)$key$value
 
-	public static AbstractMessage toObject(byte[] objectByteStream) {
+	byte[] bytes = (CLIENT_MESSAGE+LINE_FEED+message.getStatus().ordinal() + LINE_FEED
+		+ message.getKey() + LINE_FEED + message.getValue()).getBytes();
+	byte[] ctrBytes = new byte[] { RETURN };
+	byte[] tmp = new byte[bytes.length + ctrBytes.length];
+	System.arraycopy(bytes, 0, tmp, 0, bytes.length);
+	System.arraycopy(ctrBytes, 0, tmp, bytes.length, ctrBytes.length);
+	return tmp;
+    }
 
-		String message = new String(objectByteStream);
-		String[] tokens = message.split(LINE_FEED);
-		ClientMessage retrivedMessage = new ClientMessage();
-		// 0: is always the status
-		// 1: could be the key value or error
-		// 2: could be value or error
-		if (tokens[0] != null) {// should always be the status
-			int statusOrdinal = Integer.parseInt(tokens[0]);
-			retrivedMessage.setStatus(StatusType.values()[statusOrdinal]);
+    public static AbstractMessage toObject(byte[] objectByteStream) throws UnsupportedDataTypeException {
+
+	String message = new String(objectByteStream).trim();
+	String[] tokens = message.split(LINE_FEED);
+	AbstractMessage retrivedMessage = null;
+	// 0 is the message type[0,1,2]
+	if (tokens[0] != null) {
+	    AbstractMessage.MessageType messageType = toMessageType(tokens[0]);
+	    switch (messageType) {
+	    case CLIENT_MESSAGE:
+		 retrivedMessage = new ClientMessage();
+		// 1: is always the status
+		// 2: could be the key value or error
+		// 3: could be value or error
+		if (tokens[1] != null) {// should always be the status
+		    int statusOrdinal = Integer.parseInt(tokens[1]);
+		    ((ClientMessage)retrivedMessage).setStatus(StatusType.values()[statusOrdinal]);
 		}
-		if (tokens[1] != null) {// is always the key
-			retrivedMessage.setKey(tokens[1]);
+		if (tokens[2] != null) {// is always the key
+		    ((ClientMessage)retrivedMessage).setKey(tokens[2]);
 
 		}
-		if (tokens[2] != null) {
-			retrivedMessage.setValue(tokens[2].trim());
+		if (tokens[3] != null) {
+		    ((ClientMessage)retrivedMessage).setValue(tokens[3].trim());
 		}
-		return retrivedMessage;
-	}
-	
-	public static byte[] toByteArray(ServerMessage message) {
+		break;
+	    case SERVER_MESSAGE:
 		//TODO
-		return null;
+		break;
+	    case ECS_MESSAGE:
+		 retrivedMessage = new ECSMessage();
+			// 1: is always the action
+			// 2: could be the  metadata or from
+			// 3: could be to index
+		 	// 4 could be the to index
+		 	// 5: could be the to server
+		        // 6 : could be the to port
+			if (tokens.length>= 2 && tokens[1] != null) {// should always be the action
+			    int actionOrdinal = Integer.parseInt(tokens[1]);
+			    ((ECSMessage)retrivedMessage).setActionType(ECSCommand.values()[actionOrdinal]);
+			}
+			if (tokens.length>= 3 && tokens[2] != null) {// is always the key
+			    if(((ECSMessage)retrivedMessage).getActionType()== (ECSCommand.INIT) ||
+				    ((ECSMessage)retrivedMessage).getActionType()==(ECSCommand.SEND_METADATA)){
+				List<ServerInfo> metaData = getMetaData(tokens[2].trim());
+				((ECSMessage)retrivedMessage).setMetaData(metaData);
+			    }else  if(((ECSMessage)retrivedMessage).getActionType() == (ECSCommand.MOVE_DATA)){ 
+				((ECSMessage)retrivedMessage).setMoveFromIndex(tokens[2].trim());
+			    }
+			    
+			}
+			if (tokens.length>= 4 && tokens[3] != null) {// to index
+			    ((ECSMessage)retrivedMessage).setMoveToIndex(tokens[3].trim());
+			}
+			if (tokens.length>= 6 && tokens[4] != null && tokens[5] != null ) {
+			    ServerInfo toServer = new ServerInfo(tokens[4],Integer.parseInt(tokens[5]));
+			    ((ECSMessage)retrivedMessage).setMoveToServer(toServer);
+			}
+			
+		break;
+	    default:
+		    break;
+
+	    }
 	}
 	
-	public static byte[] toByteArray(ECSMessage message) {
-		//TODO
-		return null;
+	return retrivedMessage;
+    }
+
+    private static List<ServerInfo> getMetaData(String metaDataStr) {
+	List<ServerInfo> serverInfoList = new ArrayList<ServerInfo>();
+	String[] tokens = metaDataStr.split(INNER_LINE_FEED2);
+	for (String serverInfoStr : tokens) {
+	    String[] serverInfoTokens = serverInfoStr.split(INNER_LINE_FEED);
+	    ServerInfo serverInfo = new ServerInfo(serverInfoTokens[0], Integer.parseInt(serverInfoTokens[1]),serverInfoTokens[2],serverInfoTokens[3]);
+	    serverInfoList.add(serverInfo);
 	}
 	
-	
-	
-	
+	return serverInfoList;
+    }
+
+    private static MessageType toMessageType(String messageTypeStr) throws UnsupportedDataTypeException {
+
+	if (messageTypeStr.equals(CLIENT_MESSAGE))
+	    return MessageType.CLIENT_MESSAGE;
+	else if (messageTypeStr.equals(SERVER_MESSAGE))
+	    return MessageType.SERVER_MESSAGE;
+	else if (messageTypeStr.equals(ECS_MESSAGE))
+	    return MessageType.ECS_MESSAGE;
+	else 
+	    throw new UnsupportedDataTypeException("Unsupported message type");
+
+    }
+
+    public static byte[] toByteArray(ServerMessage message) {
+
+	return null;
+
+    }
+
+    public static byte[] toByteArray(ECSMessage message) {
+	// message : number(messageType)-- number(actionType)--list of meta data/fromindex--toindex-- to_serverInfo
+	String messageStr = (ECS_MESSAGE+LINE_FEED+message.getActionType().ordinal() );
+	if (message.getActionType() == ECSCommand.INIT || message.getActionType() == ECSCommand.SEND_METADATA){
+	    // add metadata
+	    messageStr += LINE_FEED;
+	    for (ServerInfo server : message.getMetaData()) {
+		messageStr += server.getAddress()+INNER_LINE_FEED+server.getPort()+INNER_LINE_FEED+server.getFromIndex()+INNER_LINE_FEED+server.getToIndex();
+		messageStr+=INNER_LINE_FEED2;
+	    }
+	    //TODO remove the last token
+
+	}else if (message.getActionType() == ECSCommand.MOVE_DATA)
+	{
+	    // add the from and to and the server info
+	    ServerInfo server = message.getMoveToServer();
+	    messageStr += LINE_FEED+ message.getMoveFromIndex()+LINE_FEED+message.getMoveToIndex()+LINE_FEED+server.getAddress()+LINE_FEED+server.getPort();
+
+	}
+
+	byte[] bytes = messageStr.getBytes();
+	byte[] ctrBytes = new byte[] { RETURN };
+	byte[] tmp = new byte[bytes.length + ctrBytes.length];
+	System.arraycopy(bytes, 0, tmp, 0, bytes.length);
+	System.arraycopy(ctrBytes, 0, tmp, bytes.length, ctrBytes.length);
+	return tmp;
+    }
 
 }
--- storage/src/testing/AdditionalTest.java
+++ storage/src/testing/AdditionalTest.java
@@ -1,167 +1,404 @@
 package testing;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import javax.activation.UnsupportedDataTypeException;
 
 import org.junit.Test;
 
+import app_kvEcs.ECSCommand;
+import app_kvEcs.ECSMessage;
 import client.SerializationUtil;
 import common.Hasher;
+import common.ServerInfo;
 import common.messages.KVMessage;
 import common.messages.KVMessage.StatusType;
 import common.messages.ClientMessage;
 import junit.framework.TestCase;
 
 public class AdditionalTest extends TestCase {
-	
-	
-	@Test
-	public void testStub() {
-		assertTrue(true);
-	}
-	
-	
-	/**
-	 * This test id for testing the serialization of <code>KVMessage</code> 
-	 * using the put function.
-	 * 
-	 * @result the <code>KVMessage</code> will be generated from the key and value
-	 * and compared with the one generated from the server
-	 */
-	@Test
-	public void testPutSerialization(){
-	    
-	    ClientMessage putMessage = new ClientMessage();
-	    putMessage.setKey("Key1");
-	    putMessage.setValue("Value1");
-	    putMessage.setStatus(StatusType.PUT);
-	    
-	    assertNotNull("Message is null",putMessage);
-	    
-	    byte[] byteStream = SerializationUtil.toByteArray(putMessage);
-	    
-	    assertNotNull("Message serialization failed."+byteStream);
-	    
-	    KVMessage deserializedMessage = (KVMessage)SerializationUtil.toObject(byteStream);
-	    
+
+
+    @Test
+    public void testStub() {
+	assertTrue(true);
+    }
+
+
+    /**
+     * This test id for testing the serialization of <code>KVMessage</code> 
+     * using the put function.
+     * 
+     * @result the <code>KVMessage</code> will be generated from the key and value
+     * and compared with the one generated from the server
+     */
+    @Test
+    public void testPutSerialization(){
+
+	ClientMessage putMessage = new ClientMessage();
+	putMessage.setKey("Key1");
+	putMessage.setValue("Value1");
+	putMessage.setStatus(StatusType.PUT);
+
+	assertNotNull("Message is null",putMessage);
+
+	byte[] byteStream = SerializationUtil.toByteArray(putMessage);
+
+	assertNotNull("Message serialization failed."+byteStream);
+
+	KVMessage deserializedMessage;
+	try {
+	    deserializedMessage = (KVMessage)SerializationUtil.toObject(byteStream);
 	    assertNotNull("Message deserialization failed.",deserializedMessage);
-	    
+	    System.out.println(putMessage.getKey()+"--"+(deserializedMessage.getKey()));
 	    assertTrue(putMessage.getKey().equals(deserializedMessage.getKey()));
 	    assertTrue(putMessage.getStatus().equals(deserializedMessage.getStatus()));
 	    assertTrue(putMessage.getValue().equals(deserializedMessage.getValue()));
-	    
+	} catch (UnsupportedDataTypeException e) {
+
+
 	}
+    }
+    
+    /**
+     * This test is for testing the serialization of <code>ECSMessage</code> 
+     * 
+     */
+    @Test
+    public void testECSInitSerialization(){
+
+	ECSMessage initMessage = new ECSMessage();
+	initMessage.setActionType(ECSCommand.INIT);
+	List<ServerInfo> metaData = new ArrayList<ServerInfo>();
+	ServerInfo s1 = new ServerInfo("1222",900,"1","10");
+	metaData.add(s1);
+	ServerInfo s2 = new ServerInfo("1333",880,"11","20");
+	metaData.add(s2);
+	ServerInfo s3 = new ServerInfo("3333",333,"33","43");
+	metaData.add(s3);
+	ServerInfo s4 = new ServerInfo("4444",666,"44","55");
+	metaData.add(s4);
+	initMessage.setMetaData(metaData);
 	
-	/**
-	 * This test id for testing the serialization of <code>KVMessage</code>
-	 * using the get function.
-	 * 
-	 * @result the <code>KVMessage</code> will be generated from the key and value
-	 * and compared with the one generated from the server
-	 */
-	@Test
-	public void testGetSerialization(){
-	    
-	    ClientMessage getMessage = new ClientMessage();
-	    getMessage.setKey("Key1");
-	    getMessage.setValue("Value1");
-	    getMessage.setStatus(StatusType.GET);
-	    
-	    assertNotNull("Message is null",getMessage);
-	    
-	    byte[] byteStream = SerializationUtil.toByteArray(getMessage);
-	    
-	    assertNotNull("Message serialization failed."+byteStream);
-	    
-	    KVMessage deserializedMessage = (KVMessage)SerializationUtil.toObject(byteStream);
-	    
+	assertNotNull("Message is null",initMessage);
+
+	byte[] byteStream = SerializationUtil.toByteArray(initMessage);
+
+	assertNotNull("Message serialization failed."+byteStream);
+
+	ECSMessage deserializedMessage;
+	try {
+	    deserializedMessage = (ECSMessage)SerializationUtil.toObject(byteStream);
 	    assertNotNull("Message deserialization failed.",deserializedMessage);
-	    
-	    assertTrue(getMessage.getKey().equals(deserializedMessage.getKey()));
-	    assertTrue(getMessage.getStatus().equals(deserializedMessage.getStatus()));
-	    assertTrue(getMessage.getValue().equals(deserializedMessage.getValue()));
-	    
+	    assertTrue(initMessage.getActionType().equals(deserializedMessage.getActionType()));
+	    assertTrue(initMessage.getMetaData().equals(deserializedMessage.getMetaData()));
+	} catch (UnsupportedDataTypeException e) {
+	    System.out.println(e.getMessage());
+
 	}
+    }
+    
+    /**
+     * This test is for testing the serialization of <code>ECSMessage</code> 
+     * 
+     */
+    @Test
+    public void testECSStartSerialization(){
+
+	ECSMessage message = new ECSMessage();
+	message.setActionType(ECSCommand.START);
 	
-	/**
-	 * This test id for testing the serialization of <code>KVMessage</code> enum types 
-	 * in this test <code>StatusType.GET_ERROR</code>
-	 * 
-	 * @result the <code>KVMessage</code> will be generated from the key and value
-	 * and compared with the one generated from the server
-	 */
-	@Test
-	public void testGetErrorSerialization(){
-	    
-	    ClientMessage getMessage = new ClientMessage();
-	    getMessage.setKey("Key1");
-	    getMessage.setValue("Value1");
-	    getMessage.setStatus(StatusType.GET_ERROR);
-	    
-	    assertNotNull("Message is null",getMessage);
-	    
-	    byte[] byteStream = SerializationUtil.toByteArray(getMessage);
-	    
-	    assertNotNull("Message serialization failed."+byteStream);
-	    
-	    KVMessage deserializedMessage = (KVMessage)SerializationUtil.toObject(byteStream);
-	    
+	
+	assertNotNull("Message is null",message);
+
+	byte[] byteStream = SerializationUtil.toByteArray(message);
+
+	assertNotNull("Message serialization failed."+byteStream);
+
+	ECSMessage deserializedMessage;
+	try {
+	    deserializedMessage = (ECSMessage)SerializationUtil.toObject(byteStream);
+	    assertNotNull("Message deserialization failed.",deserializedMessage);
+	    assertTrue(message.getActionType().equals(deserializedMessage.getActionType()));
+	} catch (UnsupportedDataTypeException e) {
+
+	}
+    }
+    
+    
+    /**
+     * This test is for testing the serialization of <code>ECSMessage</code> 
+     * 
+     */
+    @Test
+    public void testECSStopSerialization(){
+
+	ECSMessage message = new ECSMessage();
+	message.setActionType(ECSCommand.STOP);
+	
+	
+	assertNotNull("Message is null",message);
+
+	byte[] byteStream = SerializationUtil.toByteArray(message);
+
+	assertNotNull("Message serialization failed."+byteStream);
+
+	ECSMessage deserializedMessage;
+	try {
+	    deserializedMessage = (ECSMessage)SerializationUtil.toObject(byteStream);
+	    assertNotNull("Message deserialization failed.",deserializedMessage);
+	    assertTrue(message.getActionType().equals(deserializedMessage.getActionType()));
+	} catch (UnsupportedDataTypeException e) {
+
+	}
+    }
+    
+    /**
+     * This test is for testing the serialization of <code>ECSMessage</code> 
+     * 
+     */
+    @Test
+    public void testECSShutdownSerialization(){
+
+	ECSMessage message = new ECSMessage();
+	message.setActionType(ECSCommand.SHUT_DOWN);
+	
+	
+	assertNotNull("Message is null",message);
+
+	byte[] byteStream = SerializationUtil.toByteArray(message);
+
+	assertNotNull("Message serialization failed."+byteStream);
+
+	ECSMessage deserializedMessage;
+	try {
+	    deserializedMessage = (ECSMessage)SerializationUtil.toObject(byteStream);
+	    assertNotNull("Message deserialization failed.",deserializedMessage);
+	    assertTrue(message.getActionType().equals(deserializedMessage.getActionType()));
+	} catch (UnsupportedDataTypeException e) {
+
+	}
+    }
+    
+    /**
+     * This test is for testing the serialization of <code>ECSMessage</code> 
+     * 
+     */
+    @Test
+    public void testECSSetLockSerialization(){
+
+	ECSMessage message = new ECSMessage();
+	message.setActionType(ECSCommand.SET_WRITE_LOCK);
+	
+	
+	assertNotNull("Message is null",message);
+
+	byte[] byteStream = SerializationUtil.toByteArray(message);
+
+	assertNotNull("Message serialization failed."+byteStream);
+
+	ECSMessage deserializedMessage;
+	try {
+	    deserializedMessage = (ECSMessage)SerializationUtil.toObject(byteStream);
+	    assertNotNull("Message deserialization failed.",deserializedMessage);
+	    assertTrue(message.getActionType().equals(deserializedMessage.getActionType()));
+	} catch (UnsupportedDataTypeException e) {
+
+	}
+    }
+    
+    
+    
+    @Test
+    public void testECSAckSerialization(){
+
+	ECSMessage message = new ECSMessage();
+	message.setActionType(ECSCommand.ACK);
+	
+	
+	assertNotNull("Message is null",message);
+
+	byte[] byteStream = SerializationUtil.toByteArray(message);
+
+	assertNotNull("Message serialization failed."+byteStream);
+
+	ECSMessage deserializedMessage;
+	try {
+	    deserializedMessage = (ECSMessage)SerializationUtil.toObject(byteStream);
+	    assertNotNull("Message deserialization failed.",deserializedMessage);
+	    assertTrue(message.getActionType().equals(deserializedMessage.getActionType()));
+	} catch (UnsupportedDataTypeException e) {
+
+	}
+    }
+    /**
+     * This test is for testing the serialization of <code>ECSMessage</code> 
+     * 
+     */
+    @Test
+    public void testECSMoveDataSerialization(){
+
+	ECSMessage message = new ECSMessage();
+	message.setActionType(ECSCommand.MOVE_DATA);
+	message.setMoveFromIndex("10");
+	message.setMoveToIndex("20");
+	message.setMoveToServer(new ServerInfo("1009",666));
+	
+	assertNotNull("Message is null",message);
+
+	byte[] byteStream = SerializationUtil.toByteArray(message);
+
+	assertNotNull("Message serialization failed."+byteStream);
+
+	ECSMessage deserializedMessage;
+	try {
+	    deserializedMessage = (ECSMessage)SerializationUtil.toObject(byteStream);
+	    assertNotNull("Message deserialization failed.",deserializedMessage);
+	    assertTrue(message.getActionType().equals(deserializedMessage.getActionType()));
+	    assertTrue(message.getMoveFromIndex().equals(deserializedMessage.getMoveFromIndex())); 
+	    assertTrue(message.getMoveToIndex().equals(deserializedMessage.getMoveToIndex())); 
+	    assertTrue(message.getMoveToServer().equals(deserializedMessage.getMoveToServer())); 
+	} catch (UnsupportedDataTypeException e) {
+
+	}
+    }
+
+
+
+    /**
+     * This test id for testing the serialization of <code>KVMessage</code>
+     * using the get function.
+     * 
+     * @result the <code>KVMessage</code> will be generated from the key and value
+     * and compared with the one generated from the server
+     */
+    @Test
+    public void testGetSerialization(){
+
+	ClientMessage getMessage = new ClientMessage();
+	getMessage.setKey("Key1");
+	getMessage.setValue("Value1");
+	getMessage.setStatus(StatusType.GET);
+
+	assertNotNull("Message is null",getMessage);
+
+	byte[] byteStream = SerializationUtil.toByteArray(getMessage);
+
+	assertNotNull("Message serialization failed."+byteStream);
+
+	KVMessage deserializedMessage;
+	try {
+	    deserializedMessage = (KVMessage)SerializationUtil.toObject(byteStream);
 	    assertNotNull("Message deserialization failed.",deserializedMessage);
 
 	    assertTrue(getMessage.getKey().equals(deserializedMessage.getKey()));
 	    assertTrue(getMessage.getStatus().equals(deserializedMessage.getStatus()));
 	    assertTrue(getMessage.getValue().equals(deserializedMessage.getValue()));
+
+	} catch (UnsupportedDataTypeException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
-	
-	/**
-	 * This test id for testing the serialization of <code>KVMessage</code> enum types 
-	 * in this test <code>StatusType.DELETE_SUCCESS</code>
-	 * 
-	 * @result the <code>KVMessage</code> will be generated from the key and value
-	 * and compared with the one generated from the server
-	 */
-	@Test
-	public void testDeleteSerialization(){
-	    
-	    ClientMessage deleteMessage = new ClientMessage();
-	    deleteMessage.setKey("Key1");
-	    deleteMessage.setValue("Value1");
-	    deleteMessage.setStatus(StatusType.DELETE_SUCCESS);
-	    
-	    assertNotNull("Message is null",deleteMessage);
-	    
-	    byte[] byteStream = SerializationUtil.toByteArray(deleteMessage);
-	    
-	    assertNotNull("Message serialization failed."+byteStream);
-	    
-	    KVMessage deserializedMessage = (KVMessage)SerializationUtil.toObject(byteStream);
-	    
+
+
+    }
+
+    /**
+     * This test id for testing the serialization of <code>KVMessage</code> enum types 
+     * in this test <code>StatusType.GET_ERROR</code>
+     * 
+     * @result the <code>KVMessage</code> will be generated from the key and value
+     * and compared with the one generated from the server
+     */
+    @Test
+    public void testGetErrorSerialization(){
+
+	ClientMessage getMessage = new ClientMessage();
+	getMessage.setKey("Key1");
+	getMessage.setValue("Value1");
+	getMessage.setStatus(StatusType.GET_ERROR);
+
+	assertNotNull("Message is null",getMessage);
+
+	byte[] byteStream = SerializationUtil.toByteArray(getMessage);
+
+	assertNotNull("Message serialization failed."+byteStream);
+
+	KVMessage deserializedMessage;
+	try {
+	    deserializedMessage = (KVMessage)SerializationUtil.toObject(byteStream);
 	    assertNotNull("Message deserialization failed.",deserializedMessage);
-	    
-	    
+
+	    assertTrue(getMessage.getKey().equals(deserializedMessage.getKey()));
+	    assertTrue(getMessage.getStatus().equals(deserializedMessage.getStatus()));
+	    assertTrue(getMessage.getValue().equals(deserializedMessage.getValue()));
+	} catch (UnsupportedDataTypeException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+
+
+    }
+
+    /**
+     * This test id for testing the serialization of <code>KVMessage</code> enum types 
+     * in this test <code>StatusType.DELETE_SUCCESS</code>
+     * 
+     * @result the <code>KVMessage</code> will be generated from the key and value
+     * and compared with the one generated from the server
+     */
+    @Test
+    public void testDeleteSerialization(){
+
+	ClientMessage deleteMessage = new ClientMessage();
+	deleteMessage.setKey("Key1");
+	deleteMessage.setValue("Value1");
+	deleteMessage.setStatus(StatusType.DELETE_SUCCESS);
+
+	assertNotNull("Message is null",deleteMessage);
+
+	byte[] byteStream = SerializationUtil.toByteArray(deleteMessage);
+
+	assertNotNull("Message serialization failed."+byteStream);
+
+	KVMessage deserializedMessage;
+	try {
+	    deserializedMessage = (KVMessage)SerializationUtil.toObject(byteStream);
+	    assertNotNull("Message deserialization failed.",deserializedMessage);
+
+
 	    assertTrue(deleteMessage.getValue().equals(deserializedMessage.getValue()));
 	    assertTrue(deleteMessage.getKey().equals(deserializedMessage.getKey()));
 	    assertTrue(deleteMessage.getStatus().equals(deserializedMessage.getStatus()));
 
-	    
+	} catch (UnsupportedDataTypeException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
-	
-	@Test
-	public void testGetAppropirateServerHash() throws NoSuchAlgorithmException{
-		Hasher hasher = new Hasher();
-		Set<String> serverHashes = new HashSet<String> ();
-		serverHashes.add ( "10" );
-		serverHashes.add ( "20" );
-		serverHashes.add ( "30" );
-		serverHashes.add ( "40" );
-		serverHashes.add ( "50" );
-		
-		assertEquals ("error 1", "10" , hasher.getAppropriateStorageServerHash ( "5" , serverHashes ) );
-		assertEquals ("error 2", "10" , hasher.getAppropriateStorageServerHash ( "60" , serverHashes ) );
-		assertEquals ( "40" , hasher.getAppropriateStorageServerHash ( "35" , serverHashes ) );
-		assertEquals ( "40" , hasher.getAppropriateStorageServerHash ( "40" , serverHashes ) );
-	}
-	
-	
+
+
+
+    }
+
+    @Test
+    public void testGetAppropirateServerHash() throws NoSuchAlgorithmException{
+	Hasher hasher = new Hasher();
+	Set<String> serverHashes = new HashSet<String> ();
+	serverHashes.add ( "10" );
+	serverHashes.add ( "20" );
+	serverHashes.add ( "30" );
+	serverHashes.add ( "40" );
+	serverHashes.add ( "50" );
+
+	assertEquals ("error 1", "10" , hasher.getAppropriateStorageServerHash ( "5" , serverHashes ) );
+	assertEquals ("error 2", "10" , hasher.getAppropriateStorageServerHash ( "60" , serverHashes ) );
+	assertEquals ( "40" , hasher.getAppropriateStorageServerHash ( "35" , serverHashes ) );
+	assertEquals ( "40" , hasher.getAppropriateStorageServerHash ( "40" , serverHashes ) );
+    }
+
+
 }
