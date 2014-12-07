966bb890a5338873ddbb549837a90c7d997156f7
arash <arash.khataei@gmail.com>
2014-12-07 17:53:13 +0100

    Added cache and Persistent Storage functionality to the DataBaseManager.

--- storage/src/app_kvServer/DatabaseManager.java
+++ storage/src/app_kvServer/DatabaseManager.java
@@ -14,6 +14,13 @@
 
 package app_kvServer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,9 +32,54 @@ import common.messages.ClientMessage;
 
 public class DatabaseManager {
 
-	private static Map < String , String > database = new HashMap < String , String > ();
+	//private static Map < String , String > database = new HashMap < String , String > ();
+	private Cache cache;
 	private static Logger logger = Logger.getRootLogger ();
-
+	private int id;
+	private String dataBaseUri;
+	
+	public DatabaseManager(int id, int cacheSize,String cacheStrategy ){
+		
+		/* path added in order to handle invocation by a remote process through ssh
+		in order to avoid Filenotfound exception when creating Presistentstorage. 
+		Because when this program
+		is called from a remote process, the user directory will link to 
+		 "/home/<user>"
+		 it is sufficient to delete path variable from this code when there is no
+		 remote process calling this object
+		*/
+		this.id = id;
+		String path = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
+		cache = new Cache(cacheSize,Strategy.valueOf(cacheStrategy));
+		/* for local invoking of the KVserver programs(no ssh call), we remove /bin to refer the path to
+		project's root path*/ 
+		path = path.replace("/bin", "");
+		
+		/* if the name of the jar file changed! this line of code must be updated
+		for handling calls within ssh */
+		path = path.replace("ms3-server.jar", "");
+		
+		this.dataBaseUri = path +"src/app_kvServer/PersistentStorage-"+ id + ".ser"; 
+		File f = new File(dataBaseUri);
+		
+		if(!f.exists()){
+			try{
+				/* on the first run of the program the storage is created*/
+					f.createNewFile();
+					Map<String, String> temp = new HashMap<String,String>();
+					FileOutputStream fileOut = new FileOutputStream(f,false);
+					ObjectOutputStream out = new ObjectOutputStream(fileOut);
+					out.writeObject(temp);
+					logger.info("Server's First time starting: Persistent storage created!");
+					out.close();
+					fileOut.close();
+				}catch(IOException e){
+		         logger.error("an error occured while trying to create the Storage");
+		         logger.error(e.getMessage());
+		      }
+		}
+	}
+	
 	/**
 	 * put a new tuple in the database
 	 * 
@@ -35,7 +87,8 @@ public class DatabaseManager {
 	 * @param value
 	 * @return <code>KVMessage</code> return message representing the put status
 	 */
-	public static synchronized KVMessage put ( String key , String value ) {
+	@SuppressWarnings("unchecked")
+	public synchronized KVMessage put ( String key , String value ) {
 		if ( key == null ) {
 
 			ClientMessage msg = new ClientMessage ();
@@ -45,7 +98,8 @@ public class DatabaseManager {
 
 			return msg;
 
-		} else if ( database.containsKey ( key ) ) {
+		//} else if ( database.containsKey ( key ) ) {
+		} else if ( contains(key) ) {
 
 			if ( value == null || value.equals ( "null" ) ) {
 				return delete ( key );
@@ -57,7 +111,10 @@ public class DatabaseManager {
 		} else {
 			ClientMessage msg = new ClientMessage ();
 			try {
-				database.put ( key , value );
+				//database.put ( key , value );	
+				savetoDatabase(key, value);
+			    cache.push(key, value);
+			         
 				msg.setKey ( key );
 				msg.setValue ( value );
 				msg.setStatus ( common.messages.KVMessage.StatusType.PUT_SUCCESS );
@@ -80,17 +137,27 @@ public class DatabaseManager {
 	 * @param key
 	 * @return <code>KVMessage</code> return message representing the get status
 	 */
-	public static synchronized KVMessage get ( String key ) {
+	@SuppressWarnings("unchecked")
+	public synchronized  KVMessage get ( String key ) {
 		ClientMessage msg = new ClientMessage ();
 		try {
-			String value = database.get ( key );
-			if ( value == null ) {
+			
+			//String value = database.get ( key );
+			String value = cache.get(key);
+			
+			// cache miss! loading the dataBase
+			if( value == null)	
+				value = loadFromDatabase(key); 
+			    
+			// in a case the key does not exists in our database
+			 if ( value == null ) {
 				msg.setKey ( key );
 				msg.setStatus ( common.messages.KVMessage.StatusType.GET_ERROR );
 				msg.setValue ( "no value mapped to this key" );
-				logger.error ( "Error in retrieving value because no value mapped to"
-						+ " this key" );
-			} else {
+				logger.warn ( "Failed to retrieve the value because no value mapped to"
+						+  key );
+			}
+			 else {
 				msg.setKey ( key );
 				msg.setValue ( value );
 				msg.setStatus ( common.messages.KVMessage.StatusType.GET_SUCCESS );
@@ -114,10 +181,14 @@ public class DatabaseManager {
 	 * @return KVMessage containing the status of the operation if failed or
 	 *         succeed
 	 */
-	private static KVMessage delete ( String key ) {
+	@SuppressWarnings("unchecked")
+	private KVMessage delete ( String key ) {
 		ClientMessage msg = new ClientMessage ();
 		try {
-			database.remove ( key );
+			//database.remove ( key );
+			deleteFromDatabase(key);
+			cache.push(key, null);
+			
 			msg.setKey ( key );
 			msg.setStatus ( common.messages.KVMessage.StatusType.DELETE_SUCCESS );
 			logger.info ( "delete for key : '" + key + "' success" );
@@ -140,10 +211,16 @@ public class DatabaseManager {
 	 * @return KVMessage containing the status of the operation if failed or
 	 *         succeed
 	 */
-	private static KVMessage update ( String key , String value ) {
+	@SuppressWarnings("unchecked")
+	private KVMessage update ( String key , String value ) {
 		ClientMessage msg = new ClientMessage ();
 		try {
-			database.put ( key , value );
+			
+			//database.put ( key , value );
+			
+			savetoDatabase(key, value);
+		    cache.push(key, value);
+			
 			msg.setKey ( key );
 			msg.setValue ( value );
 			msg.setStatus ( common.messages.KVMessage.StatusType.PUT_UPDATE );
@@ -164,8 +241,29 @@ public class DatabaseManager {
 	 * @param data
 	 *            data collection to be inserted
 	 */
-	public synchronized static void putAll ( Map < String , String > data ) {
-		database.putAll ( data );
+	@SuppressWarnings("unchecked")
+	public synchronized void putAll ( Map < String , String > data ) {
+		//database.putAll ( data );
+		
+	    // saving the new data into DataBase
+	    try{
+	    	//loading the DataBase
+			Map<String, String> temp = new HashMap<String, String>();
+			FileInputStream fileIn = new FileInputStream(this.dataBaseUri);
+		    ObjectInputStream in = new ObjectInputStream(fileIn);
+		    temp = (HashMap<String, String>) in.readObject();
+			in.close();
+		    fileIn.close();
+		    //saving into dataBase
+	    	temp.putAll(data);
+			FileOutputStream fileOut = new FileOutputStream(this.dataBaseUri);
+		    ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		    out.writeObject(temp);
+		    out.close();
+		    fileOut.close();
+	    }catch(Exception e){
+	    	logger.error(e.getMessage());
+	    }
 	}
 
 	/**
@@ -177,16 +275,34 @@ public class DatabaseManager {
 	 *            the hash representation of the end of the range
 	 * @return Map containing the data in the range.
 	 */
-	public static Map < String , String > getDataInRange ( String rangeStart ,
+	@SuppressWarnings("unchecked")
+	public Map < String , String > getDataInRange ( String rangeStart ,
 			String rangeEnd ) {
-		Map < String , String > dataToBeMoved = new HashMap < String , String > ();
-		Hasher hasher = new Hasher ();
-		for ( String key : database.keySet () ) {
-			if ( hasher.isInRange ( rangeStart , rangeEnd , key ) ) {
-				dataToBeMoved.put ( key , database.get ( key ) );
+		
+		try{
+			//loading the DataBase
+			HashMap<String, String> temp = new HashMap<String, String>();
+			FileInputStream fileIn = new FileInputStream(this.dataBaseUri);
+		    ObjectInputStream in = new ObjectInputStream(fileIn);
+		    temp = (HashMap<String, String>) in.readObject();
+			in.close();
+		    fileIn.close();
+			
+			Map < String , String > dataToBeMoved = new HashMap < String , String > ();
+			Hasher hasher = new Hasher ();
+			//for ( String key : database.keySet () ) {
+			for ( String key : temp.keySet () ) {
+				if ( hasher.isInRange ( rangeStart , rangeEnd , key ) ) {
+					dataToBeMoved.put ( key , temp.get ( key ) );
+				}
 			}
+			
+			return dataToBeMoved;
+		}catch(Exception e){
+			logger.error("error occured while getting data"
+					+ "in range " + e.getMessage());
+			return null;
 		}
-		return dataToBeMoved;
 	}
 
 	/**
@@ -197,17 +313,128 @@ public class DatabaseManager {
 	 * @param rangeEnd
 	 *            the hash representation of the end of the range
 	 */
-	public static void removeDataInRange ( String rangeStart , String rangeEnd ) {
-		Hasher hasher = new Hasher ();
-		for ( String key : database.keySet () ) {
-			if ( hasher.isInRange ( rangeStart , rangeEnd , key ) ) {
-				database.remove ( key );
+	@SuppressWarnings("unchecked")
+	public void removeDataInRange ( String rangeStart , String rangeEnd ) {
+		
+		try{
+			//loading the DataBase
+			HashMap<String, String> temp = new HashMap<String, String>();
+			FileInputStream fileIn = new FileInputStream(this.dataBaseUri);
+		    ObjectInputStream in = new ObjectInputStream(fileIn);
+		    temp = (HashMap<String, String>) in.readObject();
+			in.close();
+		    fileIn.close();
+			
+			//Map < String , String > dataToBeMoved = new HashMap < String , String > ();
+			Hasher hasher = new Hasher ();
+			//for ( String key : database.keySet () ) {
+			for ( String key : temp.keySet () ) {
+				if ( hasher.isInRange ( rangeStart , rangeEnd , key ) ) {
+					//database.remove(key);
+					temp.remove ( key);
+				}
 			}
+			
+			//saving the changes to the persistent database
+			FileOutputStream fileOut = new FileOutputStream(this.dataBaseUri);
+		    ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		    out.writeObject(temp);
+		    out.close();
+		    fileOut.close();
+	
+		}catch(Exception e){
+			logger.error("error occured while removing data"
+					+ "in range " + e.getMessage());
+
 		}
 	}
 
-	public static void printDatabase () {
-		System.out.println ( database.toString () );
+	@SuppressWarnings("unchecked")
+	public void printDatabase () {
+		try{
+			HashMap<String, String> temp = new HashMap<String, String>();
+			FileInputStream fileIn = new FileInputStream(this.dataBaseUri);
+		    ObjectInputStream in = new ObjectInputStream(fileIn);
+		    temp = (HashMap<String, String>) in.readObject();
+			in.close();
+		    fileIn.close();
+			System.out.println ( temp.toString () );
+		}catch(Exception e){
+			logger.error("failed to print the dataBase! " + e.getMessage());
+		}
+	}
+	
+	/* additional methods added for handling input and output from the file */
+	
+	@SuppressWarnings("unchecked")
+	private void savetoDatabase(String key, String value) throws IOException, ClassNotFoundException{
+		
+		//loading the DataBase
+		HashMap<String, String> temp = new HashMap<String, String>();
+		FileInputStream fileIn = new FileInputStream(this.dataBaseUri);
+	    ObjectInputStream in = new ObjectInputStream(fileIn);
+	    temp = (HashMap<String, String>) in.readObject();
+		in.close();
+	    fileIn.close();
+	    
+	    // saving the new data into DataBase
+	    temp.put(key, value);
+	    FileOutputStream fileOut = new FileOutputStream(this.dataBaseUri);
+	    ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	    out.writeObject(temp);
+	    out.close();
+	    fileOut.close();
+	}
+	
+	@SuppressWarnings("unchecked")
+	private void deleteFromDatabase(String key) throws IOException, ClassNotFoundException{
+		
+		//loading the DataBase
+		HashMap<String, String> temp = new HashMap<String, String>();
+		FileInputStream fileIn = new FileInputStream(this.dataBaseUri);
+	    ObjectInputStream in = new ObjectInputStream(fileIn);
+	    temp = (HashMap<String, String>) in.readObject();
+		in.close();
+	    fileIn.close();
+	    
+	    // saving the new data into DataBase
+	    temp.remove(key);
+	    FileOutputStream fileOut = new FileOutputStream(this.dataBaseUri);
+	    ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	    out.writeObject(temp);
+	    out.close();
+	    fileOut.close();
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	private String loadFromDatabase(String key) throws IOException, ClassNotFoundException{
+		
+		//loading the DataBase
+		HashMap<String, String> temp = new HashMap<String, String>();
+		FileInputStream fileIn = new FileInputStream(this.dataBaseUri);
+	    ObjectInputStream in = new ObjectInputStream(fileIn);
+	    temp = (HashMap<String, String>) in.readObject();
+		in.close();
+	    fileIn.close();
+	    return temp.get(key);
+	   
+	}
+	
+	@SuppressWarnings({ "unchecked", "null" })
+	private boolean contains(String key){
+			try{
+				HashMap<String, String> temp = new HashMap<String, String>();
+				FileInputStream fileIn = new FileInputStream(this.dataBaseUri);
+			    ObjectInputStream in = new ObjectInputStream(fileIn);
+			    temp = (HashMap<String, String>) in.readObject();
+				in.close();
+			    fileIn.close();
+			    return temp.containsKey(key);
+			}catch(Exception e){
+				return false;
+			}
+				
 	}
 
 }
