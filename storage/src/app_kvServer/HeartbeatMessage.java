166e6bbb46fa6c636e1d27fff50dde6ca14645a2
ibrahim <ibrahim@ibrahim-PC>
2015-01-01 16:40:22 +0100

    added the type of heartbeat message and concrete class for the heartbeat

---
+++ storage/src/app_kvServer/HeartbeatMessage.java
@@ -0,0 +1,27 @@
+package app_kvServer;
+
+import common.ServerInfo;
+import common.messages.AbstractMessage;
+
+public class HeartbeatMessage implements AbstractMessage{
+
+	private ServerInfo coordinatorServer;
+	
+	public HeartbeatMessage(ServerInfo coordinatorServer) {
+		this.coordinatorServer = coordinatorServer;
+	}
+	
+	public void setCoordinatorServer(ServerInfo coordinatorServer) {
+		this.coordinatorServer = coordinatorServer;
+	}
+	
+	public ServerInfo getCoordinatorServer (){
+		return this.coordinatorServer;
+	}
+	 
+	@Override
+	public MessageType getMessageType() {
+		return MessageType.HEARTBEAT_MESSAGE;
+	}
+
+}
