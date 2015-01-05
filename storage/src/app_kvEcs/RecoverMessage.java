e6a4a06fa1a4015d45c117138d360ee0512d6999
arash <arash.khataei@gmail.com>
2015-01-05 19:18:46 +0100

    added recoveryMessage

---
+++ storage/src/app_kvEcs/RecoverMessage.java
@@ -0,0 +1,33 @@
+package app_kvEcs;
+
+import java.util.List;
+
+import common.ServerInfo;
+import common.messages.AbstractMessage;
+
+public class RecoverMessage implements AbstractMessage {
+
+    private ECSCommand actionType;
+    private ServerInfo failedServer;
+    
+    @Override
+    public MessageType getMessageType() {
+	return MessageType.ECS_MESSAGE;
+    }
+
+    public ECSCommand getActionType() {
+	return actionType;
+    }
+
+    public void setActionType(ECSCommand actionType) {
+	this.actionType = actionType;
+    }
+    
+    public ServerInfo getFailedServer(){
+    	return failedServer;
+    }
+    
+    public void setFailedServer(ServerInfo failed){
+    	this.failedServer = failed;
+    }
+}
